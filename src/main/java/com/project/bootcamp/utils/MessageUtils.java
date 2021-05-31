c0143b5a2ff5cb41df369bcece48c32299fcd7f3
Nilson Alves <franca.alves@gmail.com>
2021-05-30 22:36:31 -0400

    Criação de repositório com código do Bootcamp

---
+++ src/main/java/com/project/bootcamp/utils/MessageUtils.java
@@ -0,0 +1,8 @@
+package com.project.bootcamp.utils;
+
+public abstract class MessageUtils {
+    public static final String STOCK_ALREADY_EXISTS = "Stock already exists in Database!";
+
+    public static final String NO_RECORDS_FOUND = "No Record Found!";
+
+}
